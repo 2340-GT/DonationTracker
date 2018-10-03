e8723a1160ee5034771625413c1743a558cebe99
An Q Tran <anqtran@gatech.edu>
2018-10-02 22:06:23 -0400

    adding login and inside page

---
+++ app/src/main/java/gatech/a2340/donationtracker/controllers/MainActivity.java
@@ -0,0 +1,54 @@
+package gatech.a2340.donationtracker.controllers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import gatech.a2340.donationtracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Name = (EditText)findViewById(R.id.emailTextField);
+        Password = (EditText)findViewById(R.id.passwordTextField);
+        Info = (TextView)findViewById(R.id.infoTextView);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Info.setText("Number of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate (Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword) {
+        if((userName.equals("DucLe")) && (userPassword.equals("2340"))) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        } else {
+            counter--;
+
+            Info.setText("Number of attemps " + String.valueOf(counter));
+
+            if(counter == 0) {
+                Login.setEnabled(false);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/gatech/a2340/donationtracker/controllers/SecondActivity.java
@@ -0,0 +1,59 @@
+package gatech.a2340.donationtracker.controllers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import gatech.a2340.donationtracker.R;
+
+//import com.google.firebase.auth.FirebaseAuth;
+//import Firebase.auth;
+
+public class SecondActivity extends AppCompatActivity {
+
+//    private FirebaseAuth firebaseAuth;
+    private Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+//        firebaseAuth = FirebaseAuth.getInstance();
+
+        logout = (Button)findViewById(R.id.btnLogout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Logout();
+            }
+        });
+    }
+
+    public void Logout(){
+//        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.logoutMenu: {
+                Logout();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
