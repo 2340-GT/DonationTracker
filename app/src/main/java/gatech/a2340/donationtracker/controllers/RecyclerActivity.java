7ff9e666f412ecb09b32b79bbae1abfeb9b8123e
An Q Tran <anqtran@gatech.edu>
2018-10-20 21:52:28 -0400

    finished populate name list using firebase

--- app/src/main/java/gatech/a2340/donationtracker/controllers/RecyclerActivity.java
+++ app/src/main/java/gatech/a2340/donationtracker/controllers/RecyclerActivity.java
@@ -1,14 +1,19 @@
 package gatech.a2340.donationtracker.controllers;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import gatech.a2340.donationtracker.R;
 import android.support.v7.widget.RecyclerView;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -16,22 +21,41 @@ import java.util.ArrayList;
 public class RecyclerActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private static final String TAG = "RecyclerActivity";
-    private ArrayList<String> mNames = new ArrayList<>();
+    private static ArrayList<String> mNames = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mDatabase = FirebaseDatabase.getInstance().getReference();
         setContentView(R.layout.activity_locationlist);
+
         initLocationNames();
     }
 
 
     private void initLocationNames() {
-        mNames.add("An Tran");
-        mNames.add("An Nguyen");
-        mNames.add("Kiet Tran");
-        initRecyclerView();
+        mDatabase.child("locations").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot1 : dataSnapshot.getChildren()) {
+                    for (DataSnapshot snapshot : snapshot1.getChildren()) {
+                        String key = snapshot.getKey();
+                        if (key.equals("Name")) {
+                            String value = (String) snapshot.getValue();
+                            mNames.add(value);
+                        }
+                    }
+
+                }
+                initRecyclerView();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private void initRecyclerView() {
